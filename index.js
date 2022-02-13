1c25d289ded8ff4d16e327ef501f4782d37ffbe2
William Sullivan <sullivanwp95@gmail.com>
2022-02-13 01:41:20 -0500

    Adding home page view

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.render("home")
 })
 
 // Wildcard Route
