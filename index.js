ac844e327eb33af4ee7c012847a3dcfc809b19c2
William Sullivan <sullivanwp95@gmail.com>
2022-02-14 20:10:33 -0500

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+app.use(express.static('public'))
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,8 @@ function Def(html) {
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+                <link rel='stylesheet' href='/css/style.css'></link>
             </head>
             <body>
                 {html.children}
