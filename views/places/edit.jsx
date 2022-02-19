f81a5b6f39929323b6009f6ec343e01ea469d8b5
William Sullivan <sullivanwp95@gmail.com>
2022-02-19 00:49:33 -0500

    Added the edit form

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -4,7 +4,36 @@ const Def = require('../default')
 function edit (data) {
     return (
         <Def>
-            <p>Edit {data.place.name}</p>
+            <main>
+                <h1>Edit Place</h1>
+                <form action={`/places/${data.id}?_method=PUT`} method='POST'>
+                    <div className='row'>
+                        <div className='form-group col-sm-6'>
+                            <label htmlFor='name'>Place Name</label>
+                            <input className='form-control' id='name' name='name' value={data.place.name} required/>
+                        </div>
+                        <div className='form-group col-sm-6'>
+                            <label htmlFor='pic'>Place Picture</label>
+                            <input className='form-control' type='url' id='pic' name='pic' value={data.place.pic}/>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='form-group col-sm-6'>
+                            <label htmlFor='city'>City</label>
+                            <input className='form-control' id='city' name='city' value={data.place.city}/>
+                        </div>
+                        <div className='form-group col-sm-6'>
+                            <label htmlFor='state'>State</label>
+                            <input className='form-control' id='state' name='state' value={data.place.state}/>
+                        </div>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='cuisines'>Cuisines</label>
+                        <input className='form-control' id='cuisines' name='cuisines' value={data.place.cuisines} required/>
+                    </div>
+                    <input className='btn btn-primary' type='submit' value="Add Place"/>
+                </form>
+            </main>
         </Def>
     )
 }
