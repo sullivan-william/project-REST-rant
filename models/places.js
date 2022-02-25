8f600201fc0541ce7ab1ee389fabcccd1510d805
William Sullivan <sullivanwp95@gmail.com>
2022-02-25 10:40:54 -0500

    Adding showEstablished method

--- models/places.js
+++ models/places.js
@@ -25,4 +25,8 @@ const placeSchema = new mongoose.Schema({
     }
 })
 
+placeSchema.methods.showEstablished = function() {
+    return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
+
 module.exports = mongoose.model('Place', placeSchema)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,11 +7,13 @@ function show (data) {
           <main className='show-container'>
             <h1>{ data.place.name }</h1>
             <img className='show-image' src={data.place.pic} alt={data.place.name}></img>
+            <h3>Located in {data.place.city}, {data.place.state}</h3>
             <div className='show-text'>
               <h2>Rating</h2>
               <p>Not Rated</p>
               <h2>Description</h2>
-              <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
+              <h3>{data.place.showEstablished()}</h3>
+              <h4>Serving {data.place.cuisines}</h4>
             </div>
             <br></br>
             <h2>Comments</h2>
